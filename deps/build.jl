4b4e30c6f188880faeef79b12a05a04bba68d5c6
Lanfeng Pan <panlanfeng@gmail.com>
2015-09-09 14:44:30 -0500

    rm the deprecate warning in v0.4
    rm the deprecate warning in v0.4

--- deps/build.jl
+++ deps/build.jl
@@ -23,5 +23,5 @@ if !isfile(tagfile) || readchomp(tagfile) != cubvers
             run(`gcc -shared -o ../libcubature.so hcubature.o pcubature.o`)
         end
     end
-    run(`echo $cubvers` |> tagfile)
+    run(pipeline(`echo $cubvers`, tagfile))
 end
