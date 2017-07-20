fec35cb9a7a6d522b99409770306d61d729d4c6c
Steven G. Johnson <stevenj@mit.edu>
2017-07-20 13:38:49 -0400

    update URLs to 1.0.3 on github

--- deps/build.jl
+++ deps/build.jl
@@ -3,18 +3,21 @@ using Compat
 import Base.Sys.WORD_SIZE
 
 # version of cubature package to use
-cubvers="1.0.2"
+cubvers="1.0.3"
+
+# binaries url prefix
+url="https://github.com/stevengj/cubature/releases/download/v$cubvers"
 
 tagfile = "installed_vers"
 if !isfile(tagfile) || readchomp(tagfile) != "$cubvers $WORD_SIZE"
     info("Installing Cubature $cubvers library...")
     if is_windows()
-        run(download_cmd("http://ab-initio.mit.edu/cubature/libcubature$WORD_SIZE-$cubvers.dll", "libcubature.dll"))
-    elseif is_apple()
-        run(download_cmd("http://ab-initio.mit.edu/cubature/libcubature$WORD_SIZE-$cubvers.dylib", "libcubature.dylib"))
+        run(download_cmd("$url/libcubature$WORD_SIZE-$cubvers.dll", "libcubature.dll"))
+    elseif is_apple() && false
+        run(download_cmd("$url/libcubature$WORD_SIZE-$cubvers.dylib", "libcubature.dylib"))
     else
         if !isfile("cubature-$cubvers.tar.gz")
-            run(download_cmd("http://ab-initio.mit.edu/cubature/cubature-$cubvers.tgz", "cubature-$cubvers.tar.gz"))
+            run(download_cmd("https://github.com/stevengj/cubature/archive/v$cubvers.tar.gz", "cubature-$cubvers.tar.gz"))
         end
         run(unpack_cmd("cubature-$cubvers.tar.gz", ".", ".gz", ".tar"))
         cd("cubature-$cubvers") do
